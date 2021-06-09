264f1b2e81bc8322a2c9b5d84148f693cf8f7dee
MohammedOttun <ottunmo@mac.com>
2021-06-09 17:52:43 -0500

    Renamed the api controllers.
    Commented out the 'controllers/api/' routes until for now.
    
    Added get routes for these paths: /, /gallery, /contact, /forum.
    
    Commented out the 'apiRoutes' for now.

--- controllers/home-routes.js
+++
@@ -1,9 +0,0 @@
-// Get homepage
-router.get('/', async (req, res) => {
-    try {
-      // do something
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
