c89586dfb105992173de1deb7ff5f6e27d5830b9
MohammedOttun <ottunmo@mac.com>
2021-06-11 19:28:39 -0500

    Add 'SequelizeSatore' dependency.
    Created a session instance.

--- server.js
+++ server.js
@@ -4,12 +4,25 @@ const routes = require('./controllers');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const Models = require('./models/index.js');
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 const hbs = exphbs.create();
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -23,7 +36,7 @@ app.use(routes);
 
 // app.listen(PORT, () => console.log(`Now listening on ${PORT}`));
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
   
