f9d0ff1eac4566eccdba76ef27f626b9de7a7d28
MohammedOttun <ottunmo@mac.com>
2021-06-13 23:45:54 -0500

    Commented out non-working maps related code.

--- public/script.js
+++ public/script.js
@@ -40,17 +40,17 @@ $(document).ready(function () {
 			`);
     }
 
-	// Loop through Items Array and Add Another Video
+    // Loop through Items Array and Add Another Video
     function resultsLoop(data) {
-    // For Each Data Item
-    $.each(data.items, function (i, item) {
-        // Variables Displayed for Thumbnail Looped Videos
-        var thumb = item.snippet.thumbnails.medium.url;
-        var title = item.snippet.title;
-        var desc = item.snippet.description.substring(0, 150); // Up to 100 Characters
-        var vid = item.snippet.resourceId.videoId; // videoId from Console Log
+        // For Each Data Item
+        $.each(data.items, function (i, item) {
+            // Variables Displayed for Thumbnail Looped Videos
+            var thumb = item.snippet.thumbnails.medium.url;
+            var title = item.snippet.title;
+            var desc = item.snippet.description.substring(0, 150); // Up to 100 Characters
+            var vid = item.snippet.resourceId.videoId; // videoId from Console Log
 
-        $("main").append(`
+            $("main").append(`
 			<article class="item" data-key="${vid}">
 
 			<img src="${thumb}" alt="" class="thumb">
@@ -63,50 +63,50 @@ $(document).ready(function () {
         });
     }
 
-	// Click Event for Article Videos
+    // Click Event for Article Videos
     $("main").on("click", "article", function () {
         var id = $(this).attr("data-key"); // Selects "data-key" which is individual per Video
         mainVid(id); // Sends Thumb video to "Main"
     });
 
 
-// GeoLocation API -- Permission Panel Asking User for Actual Position/Location
-const successCallback = (position) => {
-    console.log(position);
-};
-// If Permission is Denied, Unavailable or Timeout
-const errorCallback = (error) => {
-    console.error(error);
-};
-// Allows Updates as Users Position Changes
-const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
+    // GeoLocation API -- Permission Panel Asking User for Actual Position/Location
+    const successCallback = (position) => {
+        console.log(position);
+    };
+    // If Permission is Denied, Unavailable or Timeout
+    const errorCallback = (error) => {
+        console.error(error);
+    };
+    // Allows Updates as Users Position Changes
+    // const watchId = navigator.geolocation.watchPosition(successCallback, errorCallback);
 
-// Dog Parks Array (using the Shortcut for a New Array = [] in scope)
-var parks = [];
-  $("#auto-complete").on("click", function (event) {
-    $("#display-results").empty();
-    parks = [];
-    let zipCode = $("#zip-input").val();
-    // Map Quest API -- Pulls information from Parameters
-    var mapquestQuery = "https://www.mapquestapi.com/geocoding/v1/address?key=d5gSCP90yDE05j7pqKiqfVqbC53mLpmC&location=" + zipCode
-    // Calling Latitude and Longitude for given zipCode
+    // Dog Parks Array (using the Shortcut for a New Array = [] in scope)
+    // var parks = [];
+    // $("#auto-complete").on("click", function (event) {
+    //     $("#display-results").empty();
+    //     parks = [];
+    //     let zipCode = $("#zip-input").val();
+    //     // Map Quest API -- Pulls information from Parameters
+    //     var mapquestQuery = "https://www.mapquestapi.com/geocoding/v1/address?key=d5gSCP90yDE05j7pqKiqfVqbC53mLpmC&location=" + zipCode
+    //     // Calling Latitude and Longitude for given zipCode
 
-    $.ajax({
-        url: mapquestQuery,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-        let lat = (response.results[0].locations[0].displayLatLng.lat);
-        let lon = (response.results[0].locations[0].displayLatLng.lng);
-        var weatherQuery = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=9252538e0cdc637a6781b199ba8f3ff7";
-     
-    // Second Call Posting Weather into Console
-    $.ajax({
-        url: weatherQuery,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response)
-    });
-    });
-    });
+    //     $.ajax({
+    //         url: mapquestQuery,
+    //         method: "GET"
+    //     }).then(function (response) {
+    //         console.log(response)
+    //         let lat = (response.results[0].locations[0].displayLatLng.lat);
+    //         let lon = (response.results[0].locations[0].displayLatLng.lng);
+    //         var weatherQuery = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=9252538e0cdc637a6781b199ba8f3ff7";
+
+    //         // Second Call Posting Weather into Console
+    //         $.ajax({
+    //             url: weatherQuery,
+    //             method: "GET"
+    //         }).then(function (response) {
+    //             console.log(response)
+    //         });
+    //     });
+    // });
 });
