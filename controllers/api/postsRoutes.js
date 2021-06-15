b2593ca957402682969b2417566cd395c0317b4d
Anna <uyenkphung@gmail.com>
2021-06-15 12:45:32 -0500

    post

--- controllers/api/postsRoutes.js
+++ controllers/api/postsRoutes.js
@@ -1,11 +1,15 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const withAuth = require('../../utils/auth');
 
 // api/posts
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const post = await Post.create(req.body);
+        const post = await Post.create(
+            req.body,
+            req.title
+            // , user_id: req.session.user_id
+            );
         console.log(post);
 
         res.status(200).json(post);
@@ -15,4 +19,5 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+module.exports = router;
 
